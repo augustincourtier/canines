21434d28619f629ca3132175ede3a82e1e0eea78
Pierre <pierre.nectoux@student.ecp.fr>
2017-02-14 17:18:40 +0100

    Merge me, i am famous.

--- The Brain/brain.py
+++ The Brain/brain.py
@@ -1,4 +1,5 @@
 import Map.map
+import numpy as np
 
 class Brain:
     def __init__(self, currentmap, side):
@@ -33,30 +34,115 @@ class Brain:
         if camp==1:
             coordX=tempmap.werewolf[i][1][0]
             coordY=tempmap.werewolf[i][1][1]
-            for j in range(Map.werewolf[i][0])
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
-                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY)]
+            for j in range(Map.werewolf[i][0]):
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX-1,coordY+1)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX+1,coordY)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX-1,coordY)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY+1)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX,coordY-1)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX+1,coordY+1)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX-1,coordY-1)]
+                maps+=[tempmap.movewerewolf(tempmap,j,coordX+1,coordY-1)]
         else:
             coordX = tempmap.vampire[i][1][0]
             coordY = tempmap.vampire[i][1][1]
-            for j in range(Map.vampire[i][0])
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
-                maps += [tempmap.movevampire(tempmap, j, coordX, coordY)]
+            for j in range(Map.vampire[i][0]):
+                maps += [tempmap.movevampire(tempmap, j, coordX+1, coordY)]
+                maps += [tempmap.movevampire(tempmap, j, coordX-1, coordY)]
+                maps += [tempmap.movevampire(tempmap, j, coordX, coordY+1)]
+                maps += [tempmap.movevampire(tempmap, j, coordX, coordY-1)]
+                maps += [tempmap.movevampire(tempmap, j, coordX+1, coordY+1)]
+                maps += [tempmap.movevampire(tempmap, j, coordX-1, coordY-1)]
+                maps += [tempmap.movevampire(tempmap, j, coordX+1, coordY-1)]
+                maps += [tempmap.movevampire(tempmap, j, coordX-1, coordY+1)]
         return maps
 
+    def probaH(self, grp1, grp2):
+        if grp1<grp2:
+            return grp1/(2*grp2)
+        elif grp1>=grp2:
+            return 1
+        else:
+            return 0
 
-    def buildTree(self):
+    def probaWV(self,grp1,grp2):
+        #TODO
+
+    def heuristic(self, map,camp):
+        H=map.humans
+        V=map.vampires
+        W=map.werewolves
+        nombreH=len(H)
+        nombreV=len(V)
+        nombreW=len(W)
+
+        #calcul des distances entre tous les groupes
+        distHW=[]
+        distVW=[]
+        distHV=[]
+        for i in range(nombreH):
+            distHW[i]=[]
+            for j in range(nombreW):
+                distHW[i][j]=max(abs(H[i][1][0]-W[i][1][0]),abs(H[i][1][1]-W[i][1][1]))
+        for i in range(nombreV):
+            distVW[i]=[]
+            for j in range(nombreW):
+                distVW[i][j] = max(abs(V[i][1][0] - W[i][1][0]), abs(V[i][1][1] - W[i][1][1]))
+        for i in range(nombreH):
+            distHV[i]=[]
+            for j in range(nombreV):
+                distHV[i][j] = max(abs(H[i][1][0] - V[i][1][0]), abs(H[i][1][1] - V[i][1][1]))
+
+        # calcul des proba de gagner les humains
+        probaW = []
+        probaV = []
+        for i in range(nombreH):
+            probaW[i] = []
+            for j in range(nombreW):
+                probaW[i][j]=self.probaH(W[i][0],H[j][0])
+        for i in range(nombreH):
+            probaV[i] = []
+            for j in range(nombreV):
+                probaV[i][j]=self.probaH(V[i][0],H[j][0])
+
+        #calcul des score
+        captureH=[]
+        for i in range(nombreH):
+            captureH[i]=[]
+            if min(distHV[i])<min(distHW):
+                captureH[i][0]='V'
+                index=self.minIndex(distHV[i])
+                captureH[i][1]=min(distHV[i])*pow(probaV[i][index],2)
+            elif min(distHV[i])>min(distHW):
+                captureH[i][0]='W'
+                index=self.minIndex(distHW[i])
+                captureH[i][1]=min(distHW[i])*pow(probaW[i][index],2)
+            else:
+                captureH[i][0]='E'
+
+
+    def minIndex(self, list):
+        index=0
+        min=0
+        for i in range(len(list)):
+            if list[i]<min:
+                index=i
+                min=list[i]
+        return index
+
+
+    def probaH(self, grp1, grp2):
+        if grp1<grp2:
+            return grp1/(2*grp2)
+        elif grp1>=grp2:
+            return 1
+        else:
+            return 0
+
+    def probaWV(self,grp1,grp2):
+        #TODO
+
+
+
 
 
