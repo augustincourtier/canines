696f1c3ab45c3a6a4810dbac62977b0c5135ad00
Unknown <mael.levet@student.ecp.fr>
2017-03-04 18:35:58 +0100

    Deleted useless function

--- src/map_generator.py
+++ src/map_generator.py
@@ -10,25 +10,20 @@ class MapGenerator:
         self.enemies_coords = enemies_coords
         self.team_possible_maps = []
 
-    def generate_maps(self, possible_moves):
-        """Method creating maps
-        :param possible_moves = [(group_id, [list of lists of possible splits],), ...]"""
-        # the length of possible_moves is the number of subgroups
-        for i in range(len(possible_moves)):
-            # iterating on all moves for all groups
-            for splits_possibility_of_group_i in possible_moves[i][1]:
-                self.team_possible_maps += [[splits_possibility_of_group_i, self.generate_maps(possible_moves[1:])]]
-        else:
-                self.team_possible_maps += [[possible_moves[0][1]]]
-
     def update_maps(self, subgroup_possible_moves):
-        """Method creating new maps according to each group possibilities"""
+        """Method creating new maps according to each group possibilities
+        :param subgroup_possible_moves = list des splits possible
+        [
+            [[nb1, [x1, y1]], [nb2, [x2, y2]]],
+                [[nb1', [x1', y1']], [nb2', [x2', y2']]], ...
+                                                                ]"""
         if len(self.team_possible_maps) == 0:
             # first iteration
             for move in subgroup_possible_moves:
                 self.team_possible_maps += [[move]]
         else:
-            maps = [[]]
+            maps = []
+            # combining each possibility of each group with each other
             for other_subgroups_move in self.team_possible_maps:
                 for move in subgroup_possible_moves:
                     maps += [[move, other_subgroups_move]]
